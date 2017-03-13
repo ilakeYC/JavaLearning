2763810c9a57f71be7afce6abd0d5335a1900d0d
iLakeYC <15076610315@sina.cn>
2017-03-13 11:09:46 +0800

    2017-03-13 11:09:43

--- Spring-01/src/Main.java
+++ Spring-01/src/Main.java
@@ -1,14 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import soundsystem.CDPlayerConfig;
+import soundsystem.CompactDisc;
+
 /**
  * @author ilakeyc
  * @since 2017/3/9
  */
+
+@ContextConfiguration(classes = CDPlayerConfig.class)
 public class Main {
 
-
+    @Autowired
+    private CompactDisc compactDisc;
 
     public static void main(String[] args) {
-
-        System.out.println("hello world!");
 
     }
 }
